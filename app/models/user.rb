1d2c04a8f9f0f7c93368e4390017feeaf11490d4
Permz <sakanaquarium0mashim0@gmail.com>
2020-10-04 15:02:29 +0900

    rails g devise:controllers users を実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
   validates :introduction, presence: false, length: { maximum: 100 } #100文字制限
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: "home#top"
   get '/' => 'home#top'
   
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
