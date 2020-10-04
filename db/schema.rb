3f1570f84b1bd326b393b3b8446de30e5d40d797
Permz <sakanaquarium0mashim0@gmail.com>
2020-10-04 13:43:34 +0900

    nameカラムをusersテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_013859) do
+ActiveRecord::Schema.define(version: 2020_10_04_043055) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
