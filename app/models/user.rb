b9bd5ef062603542d1afc42bd153b4c4df434dcd
Permz <sakanaquarium0mashim0@gmail.com>
2020-10-04 14:43:03 +0900

    usersモデルにintroductionカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :introduction, presence: false, length: { maximum: 100 } #100文字制限
 end
