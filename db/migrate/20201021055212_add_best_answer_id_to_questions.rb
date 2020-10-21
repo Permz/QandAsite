7d793ac55e134be4a6a35d897780204a148e9bb8
Permz <sakanaquarium0mashim0@gmail.com>
2020-10-21 15:41:35 +0900

    ベストアンサー機能の実装

--- db/migrate/20201021055212_add_best_answer_id_to_questions.rb
+++ db/migrate/20201021055212_add_best_answer_id_to_questions.rb
@@ -1,5 +1,5 @@
 class AddBestAnswerIdToQuestions < ActiveRecord::Migration[6.0]
   def change
-    add_column :questions, :best_flg, :boolean, default: false
+    add_column :questions, :best_answer_id, :integer, default: nil
   end
 end
