7f1084a1b52915e2d519df2553742abf273d62c8
Permz <sakanaquarium0mashim0@gmail.com>
2020-11-15 17:34:12 +0900

    meta-tag設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def default_meta_tags
       {
-        title: "介護情報共有サイト",
-        description: "介護の目的は、高齢者の尊厳の保持と、有する能力に応じて自立した日常生活を支援することにあります。目の前の利用者は何ができて何に支援が必要なのか、自分で何ができるようになりたいのか、その望みの達成のために何をすべきか、という自立支援のための視点が大切です。",
+        title: "CareYou",
+        description: "介護の目的は、高齢者の尊厳の保持と、有する能力に応じて自立した日常生活を支援することにあります。このサイトでは、介護に携わる方のために有意義な情報を発信しています。",
         keywords: "介護,共有,Q&A",
-        # favicon icon: image_url("favicon.ico")
+        icon: image_url("/favicon.ico"),
         noindex: ! Rails.env.production?, # production環境以外はnoindex  
         charset: "UTF-8",
         # OGPの設定
@@ -12,9 +12,9 @@ module ApplicationHelper
           title: "介護情報共有サイト",
           type: "website",
           url: request.original_url,
-          # favicon image: image_url("OGPで設定する画像"),
+          image: image_url("CareYou.png"),
           site_name: "CareYou",
-          description: "介護情報共有サイト",
+          description: "介護の目的は、高齢者の尊厳の保持と、有する能力に応じて自立した日常生活を支援することにあります。このサイトでは、介護に携わる方のために有意義な情報を発信しています。",
           locale: "ja_JP"
         } 
       }
